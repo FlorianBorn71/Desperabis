50f3b7a485e43d6f6a54d6a5e706fac4442a82e1
Florian Born <flborn@microsoft.com>
2024-12-29 17:22:11 +0100

    Added Desktop player

---
+++ Source/NewRenderer/DesperabisDesktopPlayer/resource.h
@@ -0,0 +1,72 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DesperabisRemotePlayer.rc
+//
+#define IDC_MYICON                      2
+#define IDD_DESPERABISREMOTEPLAYER_DIALOG 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_DESPERABISREMOTEPLAYER      107
+#define IDI_SMALL                       108
+#define IDC_DESPERABISREMOTEPLAYER      109
+#define IDR_MAINFRAME                   128
+#define ID_CAMERA_FREECAMERA            32771
+#define ID_CAMERA_FIRSTPERSONCAMERA     32772
+#define ID_CAMERA_TOGGLECAMERA          32773
+#define ID_CAMERA_THIRDPERSONCAMERA     32776
+#define ID_GAME_CREATESNAPSHOT          32777
+#define ID_ACCELERATOR32778             32778
+#define ID_RENDERER_ORIGINALASSETS      32780
+#define ID_RENDERER_WITHHIGHRES         32781
+#define ID_RENDERER_ONLYHIGHRES         32782
+#define ID_RENDERER_TOGGLEASSETMODE     32783
+#define ID_LEVEL_TRAININGSLEVEL         32786
+#define ID_LEVEL_FIRST                  32787
+#define ID_LEVEL_PART1_1                32787
+#define ID_LEVEL_PART1_2                32788
+#define ID_LEVEL_PART1_3                32789
+#define ID_LEVEL_PART1_4                32790
+#define ID_LEVEL_PART1_5                32791
+#define ID_LEVEL_PART1_6                32792
+#define ID_LEVEL_PART1_7                32793
+#define ID_LEVEL_PART2_1                32794
+#define ID_LEVEL_PART2_2                32795
+#define ID_LEVEL_PART2_3                32796
+#define ID_LEVEL_PART2_4                32797
+#define ID_LEVEL_PART2_5                32798
+#define ID_LEVEL_PART2_6                32799
+#define ID_LEVEL_PART2_7                32800
+#define ID_LEVEL_PART2_8                32801
+#define ID_LEVEL_PART2_9                32802
+#define ID_LEVEL_PART2_10               32803
+#define ID_LEVEL_PART2_11               32804
+#define ID_LEVEL_PART2_12               32805
+#define ID_LEVEL_PART2_13               32806
+#define ID_LEVEL_PART2_14               32807
+#define ID_LEVEL_PART2_15               32808
+#define ID_LEVEL_PART2_16               32809
+#define ID_LEVEL_PART2_17               32810
+#define ID_LEVEL_PART2_18               32811
+#define ID_LEVEL_LAST                   32811
+#define ID_RENDERER_USEFOG              32812
+#define ID_RENDERER_FULLBRIGHT          32813
+#define ID_RENDERER_DYNAMICLIGHTS       32814
+#define ID_RENDERER_CORONAS             32815
+#define ID_RENDERER_GRIDVISIBILITY      32816
+#define ID_REMOTECONNECTION_CONNECT     32824
+#define ID_CAMERA_GRAVITY               32825
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32828
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
